825432b910bb987be88c1c63d52b3fb49bea2562
Gergely Hegedus <hegedusgergely@yahoo.com>
2022-01-09 01:20:39 +0200

    Issue#1 Setup KtLint configuration and GitHub Action job

--- app/src/test/java/org/fnives/keepass/android/manager/ExampleUnitTest.kt
+++ app/src/test/java/org/fnives/keepass/android/manager/ExampleUnitTest.kt
@@ -2,18 +2,6 @@ package org.fnives.keepass.android.manager
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import java.io.ByteArrayOutputStream
-import java.io.IOException
-import java.io.OutputStream
-import java.io.UnsupportedEncodingException
-import java.security.DigestOutputStream
-import java.security.InvalidKeyException
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
-import javax.crypto.Cipher
-import javax.crypto.NoSuchPaddingException
-import javax.crypto.ShortBufferException
-import javax.crypto.spec.SecretKeySpec
 
 /**
  * Example local unit test, which will execute on the development machine (host).
